ca35522ab48c16c19755dec346eb7ff209c55422
Jackson Joblinske <jjoblinske@gmail.com>
2020-08-25 13:57:25 -0500

    lab01-1.c added

---
+++ Jackson_Joblinske_lab01/lab01-1.c
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------
+-		    SE 185: Lab 01 - Getting to Know the 185 Environment		 	 -
+-	Name: Jackson Joblinske																	 -
+- Section:	4															 -
+-	NetID:	195525346															     -
+-	Date:		8/25/20															 -
+-----------------------------------------------------------------------------*/
+
+/*
+	Here is a block comment.
+	These lines don't run when you compile or run the code.
+	I.e., they're the machine doesn't see them, only you.
+*/
+
+/*----------------------------------------------------------------------------
+-								Includes									 -
+-----------------------------------------------------------------------------*/
+#include <stdio.h>
+
+/*----------------------------------------------------------------------------
+-								Implementation								 -
+-----------------------------------------------------------------------------*/
+int main(int argc, char *argv[])
+{
+    // This is a C comment, this line doesn't run in the program
+
+    printf("This is a line of text"); // Modify this line
+    printf("\n"); // This prints a newline character
+
+    return 0; // This is a return statement
+}
